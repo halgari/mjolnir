6033e37d2d28599eaa6aa83b07dec23909271b7e
Timothy Baldridge <timothy@thinkrelevance.com>
2013-04-04 16:01:07 -0600

    updated project.clj

--- project.clj
+++ project.clj
@@ -4,13 +4,9 @@
   :license {:name "MIT License"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.datomic/datomic-free "0.8.3826" ]
-                 [speclj "2.5.0"]
                  [net.java.dev.jna/jna "3.4.0"]
                  [criterium "0.3.1"]
-                 [midje "1.5-RC1"]
 		 #_[jcuda/jcuda "0.5.0"]]
-
-  :plugins [[speclj "2.5.0"]]
   :test-paths ["test/"]
   :java-source-paths ["src/examples"]
   :jvm-opts ["-Djava.library.path=/usr/lib"]
